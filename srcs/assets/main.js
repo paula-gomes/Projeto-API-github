a4ac7fc0dd89297c685986fea6413a3c66b11bc3
Paula Gomes <gomes.pcristina@gmail.com>
2020-07-15 00:28:27 -0300

    acrescentando mvc de user

--- srcs/assets/main.js
+++ srcs/assets/main.js
@@ -4,8 +4,10 @@ let repoUser= document.querySelector("#repositories");
 
 submitUser.addEventListener("click", () =>{
 
+let urlUser= `https://api.github.com/users/${inputUser.value}`;
 let urlRepo= `https://api.github.com/users/${inputUser.value}/repos`; 
 
+ControllerUser.getUser(urlUser);
 ControllerRepo.getRepo(urlRepo);
 inputUser.value="";})
 
