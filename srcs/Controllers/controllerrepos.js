b73d503968e4bd524a5b4b68da0f22a086794f80
Paula Gomes <gomes.pcristina@gmail.com>
2020-07-15 20:56:41 -0300

    tratando erros

--- srcs/Controllers/controllerrepos.js
+++ srcs/Controllers/controllerrepos.js
@@ -10,14 +10,25 @@ class ControllerRepo{
     requestRepo.open("GET",urlRepo);
     requestRepo.addEventListener("load", ()=> {
 
+        try{
+            if(requestRepo.status!==200)
+
+                {throw new Error("User not found");}
+           
             let repositoriesObj=JSON.parse(requestRepo.responseText);
-            let repoUser = document.querySelector("#repositories");    
+            let repoUser = document.querySelector("#repositories");
+            repoUser.innerHTML="";   
            for(let i=0; i < repositoriesObj.length ; i++)
            {            
             let repositoryAPI= new UserGitRepo(repositoriesObj[i].name,repositoriesObj[i].html_url);               
                 repoUser.innerHTML+=RepoView.displayrepo(repositoryAPI.repoReturn());
-           }      
+           }
+        }
+        catch(e) 
+           { console.log(e);}             
    
-        });
-        requestRepo.send();
-    }}
+        });//catch(e)
+
+    requestRepo.send();
+    }
+}
--- srcs/Controllers/controlleruser.js
+++ srcs/Controllers/controlleruser.js
@@ -10,11 +10,15 @@ class ControllerUser{
     requestUser.open("GET",urluser);
     requestUser.addEventListener("load", ()=> {
 
+        if(requestUser.status!==200)
+
+        {throw new Error("User not found");}
+   
             let userObj=JSON.parse(requestUser.responseText);
             let user = document.querySelector("#userid");  
           
             let userAPI= new UserGit(userObj.avatar_url,userObj.name,userObj.bio);              
-               user.innerHTML+=UserView.displayuser(userAPI.returnUser());
+               user.innerHTML=UserView.displayuser(userAPI.returnUser());
            });   
         
     requestUser.send();
